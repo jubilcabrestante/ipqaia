b086c5f67ad08e22d0850588f68009a1c2dc96b3
Mark-Pagayon <2021100361bp@psu.palawan.edu.ph>
2025-03-14 13:27:41 +0800

    add personnel profile reports screen

---
+++ lib/features/main/personnel_profile_reports/presentation/personnel_profile_sreen_reports.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class PersonnelProfileScreenReports extends StatefulWidget {
+  const PersonnelProfileScreenReports({super.key});
+
+  @override
+  State<PersonnelProfileScreenReports> createState() =>
+      _PersonnelProfileScreenReportsState();
+}
+
+class _PersonnelProfileScreenReportsState
+    extends State<PersonnelProfileScreenReports> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Personnel Profile Reports'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Search personnel...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                  headingRowColor: WidgetStateProperty.all(Colors.redAccent),
+                  columns: const [
+                    DataColumn(
+                        label: Text('Department name',
+                            style: TextStyle(color: Colors.white))),
+                    DataColumn(
+                        label: Text('Date started',
+                            style: TextStyle(color: Colors.white))),
+                    DataColumn(
+                        label: Text('With bachelors Degree',
+                            style: TextStyle(color: Colors.white))),
+                    DataColumn(
+                        label: Text('With Masters Degree',
+                            style: TextStyle(color: Colors.white))),
+                    DataColumn(
+                        label: Text('With Masters Degree',
+                            style: TextStyle(color: Colors.white))),
+                  ],
+                  rows: const [
+                    DataRow(cells: [
+                      DataCell(Text('')),
+                      DataCell(Text('')),
+                      DataCell(Text("")),
+                      DataCell(Text('')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('')),
+                      DataCell(Text('')),
+                      DataCell(Text("")),
+                      DataCell(Text('')),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
