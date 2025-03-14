4badb244e2e300aa0c59b150bdd587bfffa13e54
Mark Andrew <132829908+Mark-Pagayon@users.noreply.github.com>
2025-03-14 14:44:29 +0800

    update

--- lib/app/routes/router.gr.dart
+++ lib/app/routes/router.gr.dart
@@ -9,451 +9,129 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i26;
-import 'package:ipqaia/app/app_viewer.dart' as _i16;
-import 'package:ipqaia/features/main/academic_offerings/presentation/academic_offerings_screen.dart'
-    as _i1;
-import 'package:ipqaia/features/main/academic_offerings/presentation/academic_reports_screen.dart'
+import 'package:auto_route/auto_route.dart' as _i8;
+import 'package:ipqaia/app/app_viewer.dart' as _i5;
+import 'package:ipqaia/features/main/account_creation/presentation/account_screen.dart'
     as _i2;
-import 'package:ipqaia/features/main/academic_offerings/presentation/programs_screen.dart'
-    as _i20;
-import 'package:ipqaia/features/main/academic_offerings/presentation/student_profile_screen.dart'
-    as _i24;
-import 'package:ipqaia/features/main/accounts/presentation/account_screen.dart'
-    as _i3;
-import 'package:ipqaia/features/main/accounts/presentation/create_account_screen.dart'
-    as _i8;
-import 'package:ipqaia/features/main/accounts/presentation/list_of_accounts_screen.dart'
-    as _i13;
-import 'package:ipqaia/features/main/accreditation/presentation/accreditation_screen.dart'
-    as _i4;
-import 'package:ipqaia/features/main/accreditation/presentation/copc_screen.dart'
-    as _i7;
-import 'package:ipqaia/features/main/accreditation/presentation/list_of_accreditation_screen.dart'
-    as _i14;
-import 'package:ipqaia/features/main/dashboard/presentation/dashboard_screen.dart'
-    as _i9;
-import 'package:ipqaia/features/main/personnel_profile/presentation/personel_profile.dart'
-    as _i18;
-import 'package:ipqaia/features/main/personnel_profile/presentation/profile_screen.dart'
-    as _i19;
-import 'package:ipqaia/features/main/personnel_profile/presentation/report_screen.dart'
-    as _i21;
-import 'package:ipqaia/features/main/sdg/presentation/asd.dart' as _i17;
-import 'package:ipqaia/features/main/sdg/presentation/list_articles_screen.dart'
-    as _i12;
-import 'package:ipqaia/features/main/sdg/presentation/sdg_screen.dart' as _i22;
-import 'package:ipqaia/features/main/students_life_and_facilties/presentation/building_screen.dart'
-    as _i5;
-import 'package:ipqaia/features/main/students_life_and_facilties/presentation/department_screen.dart'
-    as _i10;
-import 'package:ipqaia/features/main/students_life_and_facilties/presentation/slf_presentation.dart'
-    as _i23;
-import 'package:ipqaia/features/main/students_life_and_facilties/presentation/students_screen.dart'
-    as _i25;
-import 'package:ipqaia/features/onboarding/login/presentation/change_password.dart'
+import 'package:ipqaia/features/main/personel_profile/presentation/personel_profile.dart'
     as _i6;
+import 'package:ipqaia/features/main/personnel_profile_reports/presentation/personnel_profile_sreen_reports.dart'
+    as _i7;
+import 'package:ipqaia/features/onboarding/login/presentation/change_password.dart'
+    as _i1;
 import 'package:ipqaia/features/onboarding/login/presentation/forgotpasswor.dart'
-    as _i11;
+    as _i3;
 import 'package:ipqaia/features/onboarding/login/presentation/login_screen.dart'
-    as _i15;
+    as _i4;
 
 /// generated route for
-/// [_i1.AcademicOfferingsScreen]
-class AcademicOfferingsRoute extends _i26.PageRouteInfo<void> {
-  const AcademicOfferingsRoute({List<_i26.PageRouteInfo>? children})
-    : super(AcademicOfferingsRoute.name, initialChildren: children);
-
-  static const String name = 'AcademicOfferingsRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i1.AcademicOfferingsScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i2.AcademicReportsScreen]
-class AcademicReportsRoute extends _i26.PageRouteInfo<void> {
-  const AcademicReportsRoute({List<_i26.PageRouteInfo>? children})
-    : super(AcademicReportsRoute.name, initialChildren: children);
-
-  static const String name = 'AcademicReportsRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i2.AcademicReportsScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i3.AccountScreen]
-class AccountRoute extends _i26.PageRouteInfo<void> {
-  const AccountRoute({List<_i26.PageRouteInfo>? children})
-    : super(AccountRoute.name, initialChildren: children);
-
-  static const String name = 'AccountRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i3.AccountScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i4.AccreditationScreen]
-class AccreditationRoute extends _i26.PageRouteInfo<void> {
-  const AccreditationRoute({List<_i26.PageRouteInfo>? children})
-    : super(AccreditationRoute.name, initialChildren: children);
-
-  static const String name = 'AccreditationRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i4.AccreditationScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i5.BuildingScreen]
-class BuildingRoute extends _i26.PageRouteInfo<void> {
-  const BuildingRoute({List<_i26.PageRouteInfo>? children})
-    : super(BuildingRoute.name, initialChildren: children);
-
-  static const String name = 'BuildingRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i5.BuildingScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i6.ChangePassword]
-class ChangePassword extends _i26.PageRouteInfo<void> {
-  const ChangePassword({List<_i26.PageRouteInfo>? children})
+/// [_i1.ChangePassword]
+class ChangePassword extends _i8.PageRouteInfo<void> {
+  const ChangePassword({List<_i8.PageRouteInfo>? children})
     : super(ChangePassword.name, initialChildren: children);
 
   static const String name = 'ChangePassword';
 
-  static _i26.PageInfo page = _i26.PageInfo(
+  static _i8.PageInfo page = _i8.PageInfo(
     name,
     builder: (data) {
-      return const _i6.ChangePassword();
+      return const _i1.ChangePassword();
     },
   );
 }
 
 /// generated route for
-/// [_i7.CopcScreen]
-class CopcRoute extends _i26.PageRouteInfo<void> {
-  const CopcRoute({List<_i26.PageRouteInfo>? children})
-    : super(CopcRoute.name, initialChildren: children);
-
-  static const String name = 'CopcRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i7.CopcScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i8.CreateAccountScreen]
-class CreateAccountRoute extends _i26.PageRouteInfo<void> {
-  const CreateAccountRoute({List<_i26.PageRouteInfo>? children})
+/// [_i2.CreateAccountScreen]
+class CreateAccountRoute extends _i8.PageRouteInfo<void> {
+  const CreateAccountRoute({List<_i8.PageRouteInfo>? children})
     : super(CreateAccountRoute.name, initialChildren: children);
 
   static const String name = 'CreateAccountRoute';
 
-  static _i26.PageInfo page = _i26.PageInfo(
+  static _i8.PageInfo page = _i8.PageInfo(
     name,
     builder: (data) {
-      return const _i8.CreateAccountScreen();
+      return const _i2.CreateAccountScreen();
     },
   );
 }
 
 /// generated route for
-/// [_i9.DashboardScreen]
-class DashboardRoute extends _i26.PageRouteInfo<void> {
-  const DashboardRoute({List<_i26.PageRouteInfo>? children})
-    : super(DashboardRoute.name, initialChildren: children);
-
-  static const String name = 'DashboardRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i9.DashboardScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i10.DepartmentScreen]
-class DepartmentRoute extends _i26.PageRouteInfo<void> {
-  const DepartmentRoute({List<_i26.PageRouteInfo>? children})
-    : super(DepartmentRoute.name, initialChildren: children);
-
-  static const String name = 'DepartmentRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i10.DepartmentScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i11.ForgotPasswordPage]
-class ForgotPasswordRoute extends _i26.PageRouteInfo<void> {
-  const ForgotPasswordRoute({List<_i26.PageRouteInfo>? children})
+/// [_i3.ForgotPasswordPage]
+class ForgotPasswordRoute extends _i8.PageRouteInfo<void> {
+  const ForgotPasswordRoute({List<_i8.PageRouteInfo>? children})
     : super(ForgotPasswordRoute.name, initialChildren: children);
 
   static const String name = 'ForgotPasswordRoute';
 
-  static _i26.PageInfo page = _i26.PageInfo(
+  static _i8.PageInfo page = _i8.PageInfo(
     name,
     builder: (data) {
-      return const _i11.ForgotPasswordPage();
+      return const _i3.ForgotPasswordPage();
     },
   );
 }
 
 /// generated route for
-/// [_i12.ListArticlesScreen]
-class ListArticlesRoute extends _i26.PageRouteInfo<void> {
-  const ListArticlesRoute({List<_i26.PageRouteInfo>? children})
-    : super(ListArticlesRoute.name, initialChildren: children);
-
-  static const String name = 'ListArticlesRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i12.ListArticlesScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i13.ListOfAccountsScreen]
-class ListOfAccountsRoute extends _i26.PageRouteInfo<void> {
-  const ListOfAccountsRoute({List<_i26.PageRouteInfo>? children})
-    : super(ListOfAccountsRoute.name, initialChildren: children);
-
-  static const String name = 'ListOfAccountsRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i13.ListOfAccountsScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i14.ListOfAccreditationScreen]
-class ListOfAccreditationRoute extends _i26.PageRouteInfo<void> {
-  const ListOfAccreditationRoute({List<_i26.PageRouteInfo>? children})
-    : super(ListOfAccreditationRoute.name, initialChildren: children);
-
-  static const String name = 'ListOfAccreditationRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i14.ListOfAccreditationScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i15.LoginPage]
-class LoginRoute extends _i26.PageRouteInfo<void> {
-  const LoginRoute({List<_i26.PageRouteInfo>? children})
+/// [_i4.LoginPage]
+class LoginRoute extends _i8.PageRouteInfo<void> {
+  const LoginRoute({List<_i8.PageRouteInfo>? children})
     : super(LoginRoute.name, initialChildren: children);
 
   static const String name = 'LoginRoute';
 
-  static _i26.PageInfo page = _i26.PageInfo(
+  static _i8.PageInfo page = _i8.PageInfo(
     name,
     builder: (data) {
-      return const _i15.LoginPage();
+      return const _i4.LoginPage();
     },
   );
 }
 
 /// generated route for
-/// [_i16.MainAppScreen]
-class MainAppRoute extends _i26.PageRouteInfo<void> {
-  const MainAppRoute({List<_i26.PageRouteInfo>? children})
+/// [_i5.MainAppScreen]
+class MainAppRoute extends _i8.PageRouteInfo<void> {
+  const MainAppRoute({List<_i8.PageRouteInfo>? children})
     : super(MainAppRoute.name, initialChildren: children);
 
   static const String name = 'MainAppRoute';
 
-  static _i26.PageInfo page = _i26.PageInfo(
+  static _i8.PageInfo page = _i8.PageInfo(
     name,
     builder: (data) {
-      return const _i16.MainAppScreen();
+      return const _i5.MainAppScreen();
     },
   );
 }
 
 /// generated route for
-/// [_i17.ManageSdgScreen]
-class ManageSdgRoute extends _i26.PageRouteInfo<void> {
-  const ManageSdgRoute({List<_i26.PageRouteInfo>? children})
-    : super(ManageSdgRoute.name, initialChildren: children);
+/// [_i6.PersonelProfileScreen]
+class PersonelProfileRoute extends _i8.PageRouteInfo<void> {
+  const PersonelProfileRoute({List<_i8.PageRouteInfo>? children})
+    : super(PersonelProfileRoute.name, initialChildren: children);
 
-  static const String name = 'ManageSdgRoute';
+  static const String name = 'PersonelProfileRoute';
 
-  static _i26.PageInfo page = _i26.PageInfo(
+  static _i8.PageInfo page = _i8.PageInfo(
     name,
     builder: (data) {
-      return const _i17.ManageSdgScreen();
+      return const _i6.PersonelProfileScreen();
     },
   );
 }
 
 /// generated route for
-/// [_i18.PersonnelProfileScreen]
-class PersonnelProfileRoute extends _i26.PageRouteInfo<void> {
-  const PersonnelProfileRoute({List<_i26.PageRouteInfo>? children})
-    : super(PersonnelProfileRoute.name, initialChildren: children);
+/// [_i7.PersonnelProfileReportsScreen]
+class PersonnelProfileReportsRoute extends _i8.PageRouteInfo<void> {
+  const PersonnelProfileReportsRoute({List<_i8.PageRouteInfo>? children})
+    : super(PersonnelProfileReportsRoute.name, initialChildren: children);
 
-  static const String name = 'PersonnelProfileRoute';
+  static const String name = 'PersonnelProfileReportsRoute';
 
-  static _i26.PageInfo page = _i26.PageInfo(
+  static _i8.PageInfo page = _i8.PageInfo(
     name,
     builder: (data) {
-      return const _i18.PersonnelProfileScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i19.ProfileScreen]
-class ProfileRoute extends _i26.PageRouteInfo<void> {
-  const ProfileRoute({List<_i26.PageRouteInfo>? children})
-    : super(ProfileRoute.name, initialChildren: children);
-
-  static const String name = 'ProfileRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i19.ProfileScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i20.ProgramsScreen]
-class ProgramsRoute extends _i26.PageRouteInfo<void> {
-  const ProgramsRoute({List<_i26.PageRouteInfo>? children})
-    : super(ProgramsRoute.name, initialChildren: children);
-
-  static const String name = 'ProgramsRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i20.ProgramsScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i21.ReportScreen]
-class ReportRoute extends _i26.PageRouteInfo<void> {
-  const ReportRoute({List<_i26.PageRouteInfo>? children})
-    : super(ReportRoute.name, initialChildren: children);
-
-  static const String name = 'ReportRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i21.ReportScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i22.SdgScreen]
-class SdgRoute extends _i26.PageRouteInfo<void> {
-  const SdgRoute({List<_i26.PageRouteInfo>? children})
-    : super(SdgRoute.name, initialChildren: children);
-
-  static const String name = 'SdgRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i22.SdgScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i23.StudentLifeScreen]
-class StudentLifeRoute extends _i26.PageRouteInfo<void> {
-  const StudentLifeRoute({List<_i26.PageRouteInfo>? children})
-    : super(StudentLifeRoute.name, initialChildren: children);
-
-  static const String name = 'StudentLifeRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i23.StudentLifeScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i24.StudentProfileScreen]
-class StudentProfileRoute extends _i26.PageRouteInfo<void> {
-  const StudentProfileRoute({List<_i26.PageRouteInfo>? children})
-    : super(StudentProfileRoute.name, initialChildren: children);
-
-  static const String name = 'StudentProfileRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i24.StudentProfileScreen();
-    },
-  );
-}
-
-/// generated route for
-/// [_i25.StudentsScreen]
-class StudentsRoute extends _i26.PageRouteInfo<void> {
-  const StudentsRoute({List<_i26.PageRouteInfo>? children})
-    : super(StudentsRoute.name, initialChildren: children);
-
-  static const String name = 'StudentsRoute';
-
-  static _i26.PageInfo page = _i26.PageInfo(
-    name,
-    builder: (data) {
-      return const _i25.StudentsScreen();
+      return const _i7.PersonnelProfileReportsScreen();
     },
   );
 }
