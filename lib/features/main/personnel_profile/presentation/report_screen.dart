feed68b6537682b367e1963c8f92828b9ee71769
Mark Andrew <132829908+Mark-Pagayon@users.noreply.github.com>
2025-03-17 16:34:30 +0800

    update

--- lib/features/main/personnel_profile/presentation/report_screen.dart
+++ lib/features/main/personnel_profile/presentation/report_screen.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:ipqaia/app/themes/colors.dart';
+import 'package:gap/gap.dart';
+import 'package:ipqaia/core/extensions/theme_extensions.dart';
+import 'package:ipqaia/core/shared/app_custom_button.dart';
 
 @RoutePage()
 class ReportScreen extends StatefulWidget {
@@ -13,53 +15,105 @@ class ReportScreen extends StatefulWidget {
 class _ReportScreenState extends State<ReportScreen> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: DataTable(
-        columnSpacing: 20,
-        headingRowColor:
-            WidgetStateColor.resolveWith((states) => AppColors.primary),
-        columns: [
-          DataColumn(
-              label: Text('Date Started',
-                  style: TextStyle(color: AppColors.textSecondary))),
-          DataColumn(
-              label: Text('Department',
-                  style: TextStyle(color: AppColors.textSecondary))),
-          DataColumn(
-              label: Text('Degree',
-                  style: TextStyle(color: AppColors.textSecondary))),
-          DataColumn(
-              label: Text('Specialization',
-                  style: TextStyle(color: AppColors.textSecondary))),
-          DataColumn(
-              label: Text('PWD',
-                  style: TextStyle(color: AppColors.textSecondary))),
-          DataColumn(
-              label: Text('Senior Citizen',
-                  style: TextStyle(color: AppColors.textSecondary))),
-          DataColumn(
-              label: Text('Action',
-                  style: TextStyle(color: AppColors.textSecondary))),
-        ],
-        rows: [
-          DataRow(cells: [
-            DataCell(Text('June 20, 2024')),
-            DataCell(Text('CSD')),
-            DataCell(Text('Masters')),
-            DataCell(Text('Data Analyst')),
-            DataCell(Text('Yes')),
-            DataCell(Text('No')),
-            DataCell(
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.primary),
-                child: Text('Delete',
-                    style: TextStyle(color: AppColors.textSecondary)),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search article here...',
+                  prefixIcon: Icon(Icons.search),
+                ),
               ),
             ),
-          ]),
+            Gap(25),
+            Row(
+              children: [
+                Text(
+                  'COLLEGE',
+                  style: context.textTheme.bodyLarge!.copyWith(fontSize: 15),
+                ),
+                Gap(25),
+                AppCustomButton(
+                  ontab: () {},
+                  child: const Text('grabe mo ka bakla'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: DataTable(
+              columnSpacing: 100,
+              headingRowColor: WidgetStateColor.resolveWith(
+                  (states) => Colors.orange.shade400),
+              columns: const [
+                DataColumn(
+                    label: Text('Department Name',
+                        style: TextStyle(color: Colors.white))),
+                DataColumn(
+                    label: Text('Date Started',
+                        style: TextStyle(color: Colors.white))),
+                DataColumn(
+                    label: Text('With Bachelors Degree',
+                        style: TextStyle(color: Colors.white))),
+                DataColumn(
+                    label: Text('With Masters Degree',
+                        style: TextStyle(color: Colors.white))),
+                DataColumn(
+                    label: Text('With Doctorate',
+                        style: TextStyle(color: Colors.white))),
+                DataColumn(
+                    label:
+                        Text('LGBTQ', style: TextStyle(color: Colors.white))),
+                DataColumn(
+                    label: Text('Senior Citizen',
+                        style: TextStyle(color: Colors.white))),
+                DataColumn(
+                    label: Text('Male/Female',
+                        style: TextStyle(color: Colors.white))),
+                DataColumn(
+                    label: Text('Number of Personnel',
+                        style: TextStyle(color: Colors.white))),
+                DataColumn(
+                    label: Text('PWD', style: TextStyle(color: Colors.white))),
+                DataColumn(
+                    label:
+                        Text('Actions', style: TextStyle(color: Colors.white))),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(Text('bakla')),
+                  DataCell(Text('bakla')),
+                  DataCell(Text('bakla')),
+                  DataCell(Text('bakla')),
+                  DataCell(Text('bakla')),
+                  DataCell(Text('bakla')),
+                  DataCell(Text('bakla')),
+                  DataCell(Text('bakla')),
+                  DataCell(Text('bakla')),
+                  DataCell(Text('bakla')),
+                  DataCell(
+                    ElevatedButton(
+                      onPressed: () {},
+                      style:
+                          ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                      child: const Text('Delete',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+                ]),
+              ],
+            ),
+          ),
         ],
       ),
     );
