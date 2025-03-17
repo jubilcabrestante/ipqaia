aad034f96ef41e4fa7704a3e0cade3e7840dc89d
Mark Andrew <132829908+Mark-Pagayon@users.noreply.github.com>
2025-03-17 13:47:45 +0800

    update

--- lib/app/routes/router.gr.dart
+++ lib/app/routes/router.gr.dart
@@ -9,129 +9,200 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i8;
-import 'package:ipqaia/app/app_viewer.dart' as _i6;
-import 'package:ipqaia/features/main/account_creation/presentation/account_screen.dart'
-    as _i3;
-import 'package:ipqaia/features/main/accreditation/presentation/accreditation_screen.dart'
+import 'package:auto_route/auto_route.dart' as _i12;
+import 'package:ipqaia/app/app_viewer.dart' as _i8;
+import 'package:ipqaia/features/main/academic_offerings/presentation/academic_offerings_screen.dart'
     as _i1;
-import 'package:ipqaia/features/main/personel_profile/presentation/personel_profile.dart'
-    as _i7;
-import 'package:ipqaia/features/onboarding/login/presentation/change_password.dart'
+import 'package:ipqaia/features/main/account_creation/presentation/account_screen.dart'
     as _i2;
-import 'package:ipqaia/features/onboarding/login/presentation/forgotpasswor.dart'
-    as _i4;
-import 'package:ipqaia/features/onboarding/login/presentation/login_screen.dart'
+import 'package:ipqaia/features/main/accreditation/presentation/accreditation_screen.dart'
+    as _i3;
+import 'package:ipqaia/features/main/dashboard/presentation/dashboard_screen.dart'
     as _i5;
+import 'package:ipqaia/features/main/personnel_profile/presentation/personel_profile.dart'
+    as _i9;
+import 'package:ipqaia/features/main/sdg/presentation/sdg_screen.dart' as _i10;
+import 'package:ipqaia/features/main/students_life_and_facilties/presentation/slf_presentation.dart'
+    as _i11;
+import 'package:ipqaia/features/onboarding/login/presentation/change_password.dart'
+    as _i4;
+import 'package:ipqaia/features/onboarding/login/presentation/forgotpasswor.dart'
+    as _i6;
+import 'package:ipqaia/features/onboarding/login/presentation/login_screen.dart'
+    as _i7;
 
 /// generated route for
-/// [_i1.AccreditationScreen]
-class AccreditationRoute extends _i8.PageRouteInfo<void> {
-  const AccreditationRoute({List<_i8.PageRouteInfo>? children})
+/// [_i1.AcademicOfferingsScreen]
+class AcademicOfferingsRoute extends _i12.PageRouteInfo<void> {
+  const AcademicOfferingsRoute({List<_i12.PageRouteInfo>? children})
+    : super(AcademicOfferingsRoute.name, initialChildren: children);
+
+  static const String name = 'AcademicOfferingsRoute';
+
+  static _i12.PageInfo page = _i12.PageInfo(
+    name,
+    builder: (data) {
+      return const _i1.AcademicOfferingsScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [_i2.AccountScreen]
+class AccountRoute extends _i12.PageRouteInfo<void> {
+  const AccountRoute({List<_i12.PageRouteInfo>? children})
+    : super(AccountRoute.name, initialChildren: children);
+
+  static const String name = 'AccountRoute';
+
+  static _i12.PageInfo page = _i12.PageInfo(
+    name,
+    builder: (data) {
+      return const _i2.AccountScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [_i3.AccreditationScreen]
+class AccreditationRoute extends _i12.PageRouteInfo<void> {
+  const AccreditationRoute({List<_i12.PageRouteInfo>? children})
     : super(AccreditationRoute.name, initialChildren: children);
 
   static const String name = 'AccreditationRoute';
 
-  static _i8.PageInfo page = _i8.PageInfo(
+  static _i12.PageInfo page = _i12.PageInfo(
     name,
     builder: (data) {
-      return const _i1.AccreditationScreen();
+      return const _i3.AccreditationScreen();
     },
   );
 }
 
 /// generated route for
-/// [_i2.ChangePassword]
-class ChangePassword extends _i8.PageRouteInfo<void> {
-  const ChangePassword({List<_i8.PageRouteInfo>? children})
+/// [_i4.ChangePassword]
+class ChangePassword extends _i12.PageRouteInfo<void> {
+  const ChangePassword({List<_i12.PageRouteInfo>? children})
     : super(ChangePassword.name, initialChildren: children);
 
   static const String name = 'ChangePassword';
 
-  static _i8.PageInfo page = _i8.PageInfo(
+  static _i12.PageInfo page = _i12.PageInfo(
     name,
     builder: (data) {
-      return const _i2.ChangePassword();
+      return const _i4.ChangePassword();
     },
   );
 }
 
 /// generated route for
-/// [_i3.CreateAccountScreen]
-class CreateAccountRoute extends _i8.PageRouteInfo<void> {
-  const CreateAccountRoute({List<_i8.PageRouteInfo>? children})
-    : super(CreateAccountRoute.name, initialChildren: children);
+/// [_i5.DashboardScreen]
+class DashboardRoute extends _i12.PageRouteInfo<void> {
+  const DashboardRoute({List<_i12.PageRouteInfo>? children})
+    : super(DashboardRoute.name, initialChildren: children);
 
-  static const String name = 'CreateAccountRoute';
+  static const String name = 'DashboardRoute';
 
-  static _i8.PageInfo page = _i8.PageInfo(
+  static _i12.PageInfo page = _i12.PageInfo(
     name,
     builder: (data) {
-      return const _i3.CreateAccountScreen();
+      return const _i5.DashboardScreen();
     },
   );
 }
 
 /// generated route for
-/// [_i4.ForgotPasswordPage]
-class ForgotPasswordRoute extends _i8.PageRouteInfo<void> {
-  const ForgotPasswordRoute({List<_i8.PageRouteInfo>? children})
+/// [_i6.ForgotPasswordPage]
+class ForgotPasswordRoute extends _i12.PageRouteInfo<void> {
+  const ForgotPasswordRoute({List<_i12.PageRouteInfo>? children})
     : super(ForgotPasswordRoute.name, initialChildren: children);
 
   static const String name = 'ForgotPasswordRoute';
 
-  static _i8.PageInfo page = _i8.PageInfo(
+  static _i12.PageInfo page = _i12.PageInfo(
     name,
     builder: (data) {
-      return const _i4.ForgotPasswordPage();
+      return const _i6.ForgotPasswordPage();
     },
   );
 }
 
 /// generated route for
-/// [_i5.LoginPage]
-class LoginRoute extends _i8.PageRouteInfo<void> {
-  const LoginRoute({List<_i8.PageRouteInfo>? children})
+/// [_i7.LoginPage]
+class LoginRoute extends _i12.PageRouteInfo<void> {
+  const LoginRoute({List<_i12.PageRouteInfo>? children})
     : super(LoginRoute.name, initialChildren: children);
 
   static const String name = 'LoginRoute';
 
-  static _i8.PageInfo page = _i8.PageInfo(
+  static _i12.PageInfo page = _i12.PageInfo(
     name,
     builder: (data) {
-      return const _i5.LoginPage();
+      return const _i7.LoginPage();
     },
   );
 }
 
 /// generated route for
-/// [_i6.MainAppScreen]
-class MainAppRoute extends _i8.PageRouteInfo<void> {
-  const MainAppRoute({List<_i8.PageRouteInfo>? children})
+/// [_i8.MainAppScreen]
+class MainAppRoute extends _i12.PageRouteInfo<void> {
+  const MainAppRoute({List<_i12.PageRouteInfo>? children})
     : super(MainAppRoute.name, initialChildren: children);
 
   static const String name = 'MainAppRoute';
 
-  static _i8.PageInfo page = _i8.PageInfo(
+  static _i12.PageInfo page = _i12.PageInfo(
     name,
     builder: (data) {
-      return const _i6.MainAppScreen();
+      return const _i8.MainAppScreen();
     },
   );
 }
 
 /// generated route for
-/// [_i7.PersonelProfileScreen]
-class PersonelProfileRoute extends _i8.PageRouteInfo<void> {
-  const PersonelProfileRoute({List<_i8.PageRouteInfo>? children})
-    : super(PersonelProfileRoute.name, initialChildren: children);
+/// [_i9.PersonnelProfileScreen]
+class PersonnelProfileRoute extends _i12.PageRouteInfo<void> {
+  const PersonnelProfileRoute({List<_i12.PageRouteInfo>? children})
+    : super(PersonnelProfileRoute.name, initialChildren: children);
 
-  static const String name = 'PersonelProfileRoute';
+  static const String name = 'PersonnelProfileRoute';
 
-  static _i8.PageInfo page = _i8.PageInfo(
+  static _i12.PageInfo page = _i12.PageInfo(
     name,
     builder: (data) {
-      return const _i7.PersonelProfileScreen();
+      return const _i9.PersonnelProfileScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [_i10.SdgScreen]
+class SdgRoute extends _i12.PageRouteInfo<void> {
+  const SdgRoute({List<_i12.PageRouteInfo>? children})
+    : super(SdgRoute.name, initialChildren: children);
+
+  static const String name = 'SdgRoute';
+
+  static _i12.PageInfo page = _i12.PageInfo(
+    name,
+    builder: (data) {
+      return const _i10.SdgScreen();
+    },
+  );
+}
+
+/// generated route for
+/// [_i11.StudentLifeScreen]
+class StudentLifeRoute extends _i12.PageRouteInfo<void> {
+  const StudentLifeRoute({List<_i12.PageRouteInfo>? children})
+    : super(StudentLifeRoute.name, initialChildren: children);
+
+  static const String name = 'StudentLifeRoute';
+
+  static _i12.PageInfo page = _i12.PageInfo(
+    name,
+    builder: (data) {
+      return const _i11.StudentLifeScreen();
     },
   );
 }
